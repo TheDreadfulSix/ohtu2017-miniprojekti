bc28bf90713d45011d5b23499afc2978c95eb259
zeroniv <eskelinenn.sami@gmail.com>
2017-04-17 12:42:54 +0300

    Reference - abstract class

--- src/main/java/miniprojekti/domain/Article.java
+++ src/main/java/miniprojekti/domain/Article.java
@@ -2,121 +2,40 @@ package miniprojekti.domain;
 
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Contains reference information of an article.
  */
-public class Article {
-    private static final Set<FieldName> requiredFields = EnumSet.of(
-        FieldName.AUTHOR, 
-        FieldName.JOURNAL, 
-        FieldName.TITLE, 
-        FieldName.YEAR
-    );
-    
-    private static final Set<FieldName> optionalFields = EnumSet.of(
-        FieldName.MONTH, 
-        FieldName.NOTE, 
-        FieldName.NUMBER, 
-        FieldName.PAGES,
-        FieldName.VOLUME
-    );
-    
-    private String citationKey;
-    
-    private Map<FieldName, Field> fields = new HashMap<>();
+public class Article extends Reference {
+    static {
+        requiredFields = EnumSet.of(
+            FieldName.AUTHOR, 
+            FieldName.JOURNAL, 
+            FieldName.TITLE, 
+            FieldName.YEAR
+        );
+        optionalFields = EnumSet.of(
+            FieldName.MONTH, 
+            FieldName.NOTE, 
+            FieldName.NUMBER, 
+            FieldName.PAGES,
+            FieldName.VOLUME
+        );
+    }
     
     /**
-     * Initialization with a collection of fields instead of a map, calls
-     * {@link #Article(java.lang.String, java.util.Map)}.
-     * 
-     * @see #Article(java.lang.String, java.util.Map)
+     * Calls {@link #Reference(java.lang.String, java.util.Collection)}.
      */
     public Article(String citationKey, Collection<Field> fields) {
-        this(citationKey, fieldMap(fields));
+        this(citationKey, createFieldMap(fields));
     }
     
     /**
-     * Initializes the article with given fields if they are valid. 
-     * 
-     * @param   citationKey
-     * @param   fields
-     * 
-     * @see miniprojekti.domain.Field
-     * @see miniprojekti.domain.FieldName
-     * 
-     * @throws IllegalArgumentException on missing required fields
-     * @throws IllegalArgumentException on invalid optional fields
+     * Calls {@link #Reference(java.lang.String, java.util.Map)}.
      */
     public Article(String citationKey, Map<FieldName, Field> fields) throws IllegalArgumentException {
-        if (!fields.keySet().containsAll(requiredFields)) {
-            throw new IllegalArgumentException("Required fields are missing");
-        }
-        
-        if (!this.getAllFieldNames().containsAll(fields.keySet())) {
-            throw new IllegalArgumentException("Invalid optional fields");
-        }
-        
-        this.citationKey = citationKey;
-        this.fields = fields;
+        super(citationKey, fields);
     }
     
-    /**
-     * Gets the field of given fieldname.
-     * 
-     * @param   name
-     * 
-     * @see miniprojekti.domain.Field
-     * @see miniprojekti.domain.FieldName
-     * 
-     * @return Fieldname if one exists, otherwise null.
-     */
-    public Field getField(FieldName name) {
-        return fields.getOrDefault(name, null);
-    }
-
-    public String getCitationKey() {
-        return citationKey;
-    }
-    
-    public Map<FieldName, Field> getFieldMap() {
-        return fields;
-    }
-    
-    public static Set<FieldName> getRequiredFields() {
-        return requiredFields;
-    }
-    
-    public static Set<FieldName> getOptionalFields() {
-        return optionalFields;
-    }
-    
-    private static Map<FieldName, Field> fieldMap(Collection<Field> fields) {
-        Map<FieldName, Field> fieldMap = new HashMap<>();
-        
-        for (Field field: fields) {
-            fieldMap.put(field.getName(), field);
-        }
-        
-        return fieldMap;
-    }
-    
-    private Set<FieldName> getAllFieldNames() {
-        return Stream.concat(optionalFields.stream(), requiredFields.stream())
-               .collect(Collectors.toSet());
-    }
-    
-    public String toString() {
-        String s = "source: Article\ncitation key: " + citationKey + "\n";
-        for(FieldName fn : fields.keySet()){
-            s += fn.name().toLowerCase() + ": " + fields.get(fn).getValue() + "\n";
-        }
-        s += "\n\n";
-        return s;
-    }
 }
---
+++ src/main/java/miniprojekti/domain/Reference.java
@@ -0,0 +1,111 @@
+package miniprojekti.domain;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public abstract class Reference {
+    protected String citationKey;
+    
+    protected static Set<FieldName> requiredFields;
+    protected static Set<FieldName> optionalFields;
+    
+    protected Map<FieldName, Field> fields;
+    
+    /**
+     * Initialization with a collection of fields instead of a map, calls
+     * {@link #Reference(java.lang.String, java.util.Map)} after creating the
+     * map.
+     * 
+     * @param   citationKey
+     * @param   fields
+     * 
+     * @see #Article(java.lang.String, java.util.Map)
+     */
+    public Reference(String citationKey, Collection<Field> fields) {
+        this(citationKey, createFieldMap(fields));
+    }
+    
+    /**
+     * Initializes the reference with given citation key and fields.
+     * 
+     * @param   citationKey
+     * @param   fields
+     * 
+     * @see miniprojekti.domain.Field
+     * @see miniprojekti.domain.FieldName
+     * 
+     * @throws IllegalArgumentException on missing required fields
+     * @throws IllegalArgumentException on invalid optional fields
+     */
+    public Reference(String citationKey, Map<FieldName, Field> fields) throws IllegalArgumentException {
+        if (!fields.keySet().containsAll(requiredFields)) {
+            throw new IllegalArgumentException("Required fields are missing");
+        }
+        
+        if (!this.getAllFieldNames().containsAll(fields.keySet())) {
+            throw new IllegalArgumentException("Invalid optional fields");
+        }
+        
+        this.citationKey = citationKey;
+        this.fields = fields;
+    }
+    
+    /**
+     * Gets the field of given fieldname.
+     * 
+     * @param   name
+     * 
+     * @see miniprojekti.domain.Field
+     * @see miniprojekti.domain.FieldName
+     * 
+     * @return Fieldname if one exists, otherwise null.
+     */
+    public Field getField(FieldName name) {
+        return fields.getOrDefault(name, null);
+    }
+    
+    public String getCitationKey() {
+        return citationKey;
+    }
+    
+    public Map<FieldName, Field> getFieldMap() {
+        return fields;
+    }
+    
+    public static Set<FieldName> getRequiredFields() {
+        return requiredFields;
+    }
+    
+    public static Set<FieldName> getOptionalFields() {
+        return optionalFields;
+    }
+
+    protected Set<FieldName> getAllFieldNames() {
+        return Stream.concat(getOptionalFields().stream(), getRequiredFields().stream())
+               .collect(Collectors.toSet());
+    }
+
+    protected static Map<FieldName, Field> createFieldMap(Collection<Field> fields) {
+        Map<FieldName, Field> fieldMap = new HashMap<>();
+        
+        for (Field field: fields) {
+            fieldMap.put(field.getName(), field);
+        }
+        
+        return fieldMap;
+    }
+    
+    @Override
+    public String toString() {
+        String s = "source: Article\ncitation key: " + citationKey + "\n";
+        for(FieldName fn : fields.keySet()){
+            s += fn.name().toLowerCase() + ": " + fields.get(fn).getValue() + "\n";
+        }
+        s += "\n\n";
+        return s;
+    }
+}
