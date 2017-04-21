b450b4671fccbe3d1ef956d0498b747b88b65fd4
ValtteriV <valtteri.vistiaho@gmail.com>
2017-04-21 10:41:00 +0300

    create.sql fix and working jar

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,1 +1,6 @@
-CREATE TABLE IF NOT EXISTS references(citationKey VARCHAR(255) PRIMARY KEY, author VARCHAR(255), title VARCHAR(255), journal VARCHAR(255), year VARCHAR(255), volume VARCHAR(255), number VARCHAR(255), pages VARCHAR(255), month VARCHAR(255), note VARCHAR(255), publisher VARCHAR(255), series VARCHAR(255), class VARCHAR(255), editor VARCHAR(255), address VARCHAR(255), edition VARCHAR(255), howpublished VARCHAR(255), type VARCHAR(255), organization VARCHAR(255), school VARCHAR(255), booktitle VARCHAR(255);
+CREATE TABLE IF NOT EXISTS references(citationKey VARCHAR(255) PRIMARY KEY,
+ author VARCHAR(255), title VARCHAR(255), journal VARCHAR(255), year VARCHAR(255),
+ volume VARCHAR(255), number VARCHAR(255), pages VARCHAR(255), month VARCHAR(255),
+ note VARCHAR(255), publisher VARCHAR(255), series VARCHAR(255), class VARCHAR(255),
+ editor VARCHAR(255), address VARCHAR(255), edition VARCHAR(255), howpublished VARCHAR(255),
+ type VARCHAR(255), organization VARCHAR(255), school VARCHAR(255), booktitle VARCHAR(255));
