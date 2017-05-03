cb70eb8537caa119133d1b49f4a1f7772e9eb9e0
esksami <eskelinenn.sami@gmail.com>
2017-05-03 04:45:33 +0300

    refaktorointia ja tyylimuokkauksia

--- src/main/java/miniprojekti/gui/GUI.java
+++ src/main/java/miniprojekti/gui/GUI.java
@@ -5,16 +5,24 @@
  */
 package miniprojekti.gui;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import miniprojekti.domain.Reference;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import miniprojekti.main.App;
@@ -39,16 +47,19 @@ public class GUI {
      */
     public void setScene() {
         VBox layout = new VBox();
-        ScrollPane sp = new ScrollPane(createPane());
+        layout.getStyleClass().add("referenceView");
+        ScrollPane sp = new ScrollPane(createVPane());
         sp.setFitToWidth(true);
         sp.setFitToHeight(true);
         
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(new ActionMenu());
+        menuBar.getStyleClass().add("menuBar");
         
         layout.getChildren().addAll(menuBar, Filter.createFilterBar(), sp);
         scene = new Scene(layout, 1240, 720);
         scene.getStylesheets().add("style.css");
+        
         sp.requestFocus();
         App.setScene(scene);
     }
@@ -60,57 +71,50 @@ public class GUI {
     public Scene getScene() {
         return this.scene;
     }
-
-    public GridPane createPane() {
-        pane = new GridPane();
+    
+    public VBox createVPane() {
+        VBox referenceView = new VBox();
+        referenceView.setPadding(new Insets(5));
+        referenceView.setSpacing(8);
+        referenceView.getStyleClass().add("referenceView");
+        
         ObservableList<Reference> oList = FXCollections.observableArrayList(App.getLogic().getList());
-        int lineCounter = 0;
-        for (Reference ref : oList) {
-            lineCounter++;
-            
-            if(lineCounter > 0){
-                GridPane innerPane = new GridPane();
-                innerPane.setHgap(3);
-                ColumnConstraints col1 = new ColumnConstraints();
-                col1.setPercentWidth(90);
-                col1.fillWidthProperty();
-                ColumnConstraints col2 = new ColumnConstraints();
-                col2.setPercentWidth(5);
-                col2.fillWidthProperty();
-                ColumnConstraints col3 = new ColumnConstraints();
-                col3.setPercentWidth(5);
-                col3.fillWidthProperty();
-                innerPane.getColumnConstraints().addAll(col1,col2,col3);
-                
-                Text referenceText = new Text(ref.toString());
-                Button deleteButton = new Button("Delete");
-                Button editButton = new Button("Edit");
-                
-                deleteButton.setOnAction(e -> {
-                    if(alertG.alertWithChoice("Confirm deletion", "Are you sure?")) {
-                        //DELETE
-                        App.getLogic().delete(ref);
-                        setScene();
-                    }
-                    //DONT DELETE
-                });
-                
-                editButton.setOnAction((ActionEvent event) -> {
-                    //TODO edit ref. Not part of Sprint 3.
-                    EditReference edit = new EditReference();
-                    edit.display(ref);
-                });
-                
-                innerPane.add(referenceText, 0, 0);
-                innerPane.add(editButton, 1, 0);
-                innerPane.add(deleteButton, 2, 0);
-                innerPane.getStyleClass().add("innerCell");
-                
-                pane.add(innerPane, 0, lineCounter);// (item, column, row)
-                pane.getStyleClass().add("outterGrid");
-            }
-        }
-        return pane;
-    }
 
+        Text title = new Text("References");
+        title.setStyle("-fx-fill: linear-gradient(from 0% 0% to 200% 100%, repeat, #55AABB 0%, #AA0000 50%);-fx-font-size:30px;");
+        referenceView.getChildren().add(title);
+        
+        for (Reference ref : oList) {
+            HBox referenceItem = new HBox();
+            referenceItem.setPadding(new Insets(5, 5, 5, 5));
+            referenceItem.setSpacing(10);
+            referenceItem.getStyleClass().add("referenceItem");
+            
+            Text referenceText = new Text(ref.toString());
+            Button deleteButton = new Button("Delete");
+            Button editButton = new Button("Edit");
+
+            deleteButton.setOnAction(e -> {
+                if(alertG.alertWithChoice("Confirm deletion", "Are you sure?")) {
+                    App.getLogic().delete(ref);
+                    setScene();
+                }
+            });
+
+            editButton.setOnAction((ActionEvent event) -> {
+                EditReference edit = new EditReference();
+                edit.display(ref);
+            });
+
+            Pane spacer = new Pane();
+            HBox.setHgrow(spacer, Priority.ALWAYS);
+            spacer.setMinSize(10, 1);
+            
+
+            referenceItem.getChildren().addAll(referenceText, spacer, deleteButton, editButton);
+            referenceView.getChildren().add(referenceItem);
+        }
+        
+        return referenceView;
+    }
 }
